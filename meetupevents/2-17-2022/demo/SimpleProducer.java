5240b9f5067a2a217ef9a38e50bd668dfcd1d971
Rittika Adhikari <rittika.adhikari@gmail.com>
2022-02-17 18:18:31 -0800

    uploading demo with a SimpleProducer

---
+++ meetupevents/2-17-2022/demo/SimpleProducer.java
@@ -0,0 +1,30 @@
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SimpleProducer {
+   
+   public static void main(String[] args) throws Exception{
+      
+      String topicName = "quickstart-events";
+
+      // setup producer configs
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092"); // broker address
+
+      // create producer object
+      Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+      // create dummy record to send
+      for (int i = 0; i < 10; i++) {
+          ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName, "Hello world!", "Women Who Code");
+          producer.send(record);
+          TimeUnit.SECONDS.sleep(1);
+      }
+      
+      // gracefully close
+      producer.close();
+   }
+}
